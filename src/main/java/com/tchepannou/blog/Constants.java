a0a7e8d2467cfd217d29d6b44968adf84f725660
htchepannou <htchepannou@expedia.com>
2015-08-28 17:55:35 -0400

    remove auth metrics

--- src/main/java/com/tchepannou/blog/Constants.java
+++ src/main/java/com/tchepannou/blog/Constants.java
@@ -8,14 +8,6 @@ public class Constants {
     public static final String METRIC_UPDATE_TEXT = "blog-update-post-text";
     public static final String METRIC_DELETE_POST = "blog-delete-post";
 
-    public static final String METRIC_AUTH_COUNT = "blog-auth-count";
-    public static final String METRIC_AUTH_ERRORS = "blog-auth-errors";
-    public static final String METRIC_AUTH_DURATION = "blog-auth-duration";
-
-    public static final String METRIC_PERMISSION_COUNT = "blog-permission-count";
-    public static final String METRIC_PERMISSION_ERRORS = "blog-permission-errors";
-    public static final String METRIC_PERMISSION_DURATION = "blog-permission-duration";
-
     public static final String QUEUE_EVENT_LOG = "blog-queue-event-log";
     public static final String EVENT_CREATE_TEXT = "blog-create-post-text";
     public static final String EVENT_UPDATE_TEXT = "blog-update-post-text";
